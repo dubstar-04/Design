58a95192b49456d967b92de5b916be703f9f37f5
Brett-Parker <brettleon123@gmail.com>
2023-02-08 22:27:35 +0000

    Added region to canvas

--- src/Design/canvas.js
+++ src/Design/canvas.js
@@ -67,6 +67,7 @@ export const Canvas = GObject.registerClass({
     keyController.connect('key-pressed', this.on_key_press.bind(this));
     this.add_controller(keyController);
 
+    // #region CTRL + '' shortcuts
     const shortcutController = new Gtk.ShortcutController();
 
     const copyShortcut = new Gtk.Shortcut({trigger: Gtk.ShortcutTrigger.parse_string('<Primary>C'), action: Gtk.CallbackAction.new(this.on_copy.bind(this))});
@@ -79,6 +80,7 @@ export const Canvas = GObject.registerClass({
     shortcutController.add_shortcut(undoShortcut);
 
     this.add_controller(shortcutController);
+    // #endregion
 
     this.core = new Core();
 
