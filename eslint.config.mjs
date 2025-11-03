d7eae074e9abe0537e941896eefd27ced808a447
Daniel Wood <s.d.wood.82@googlemail.com>
2025-11-03 08:12:03 +0000

    Migrate eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([{
+    extends: compat.extends("google"),
+    plugins: {},
+
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+        },
+
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+
+    settings: {},
+
+    rules: {
+        "require-jsdoc": 0,
+        "valid-jsdoc": 0,
+        "max-len": 0,
+    },
+}]);
