1f236560acbfdc5d510b6086f44a3499b8e1b653
Daniel Wood <s.d.wood.82@googlemail.com>
2025-11-03 08:20:53 +0000

    fix lint warnings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,36 +1,36 @@
-import { defineConfig } from "eslint/config";
-import globals from "globals";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import {defineConfig} from 'eslint/config';
+import globals from 'globals';
+import path from 'node:path';
+import {fileURLToPath} from 'node:url';
+import js from '@eslint/js';
+import {FlatCompat} from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
 export default defineConfig([{
-    extends: compat.extends("google"),
-    plugins: {},
+  extends: compat.extends('google'),
+  plugins: {},
 
-    languageOptions: {
-        globals: {
-            ...globals.browser,
-        },
-
-        ecmaVersion: "latest",
-        sourceType: "module",
+  languageOptions: {
+    globals: {
+      ...globals.browser,
     },
 
-    settings: {},
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
 
-    rules: {
-        "require-jsdoc": 0,
-        "valid-jsdoc": 0,
-        "max-len": 0,
-    },
+  settings: {},
+
+  rules: {
+    'require-jsdoc': 0,
+    'valid-jsdoc': 0,
+    'max-len': 0,
+  },
 }]);
