6eb342967e2cc251d158c38a999013fd88dec669
Daniel Wood <s.d.wood.82@googlemail.com>
2025-11-11 18:59:04 +0000

    remove unused import

--- src/Design/Preferences/preferencesDialog.js
+++ src/Design/Preferences/preferencesDialog.js
@@ -18,7 +18,6 @@
 
 import GObject from 'gi://GObject';
 import Adw from 'gi://Adw?version=1';
-// import Gio from 'gi://Gio';
 
 import { PreferencePageSettings } from './preferencePageSettings.js';
 import { PreferencePageTextStyle } from './preferencePageTextStyle.js';
