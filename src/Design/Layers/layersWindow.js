fe9a077f06d569bf895346a681886fe257cd0f2a
Daniel Wood <s.d.wood.82@googlemail.com>
2025-11-11 19:01:06 +0000

    update layers UI to use common widgets
    fixes #316

--- src/Design/Layers/layersWindow.js
+++ src/Design/Layers/layersWindow.js
@@ -20,70 +20,29 @@ import GObject from 'gi://GObject';
 import Adw from 'gi://Adw?version=1';
 import Gtk from 'gi://Gtk';
 import Gdk from 'gi://Gdk';
-import Gio from 'gi://Gio';
-import GLib from 'gi://GLib';
 
 import { Colours } from '../../Design-Core/core/lib/colours.js';
 import { DesignCore } from '../../Design-Core/core/designCore.js';
 
+import { PreferenceRow } from '../Widgets/preferenceRow.js';
+
 export const LayersWindow = GObject.registerClass({
   GTypeName: 'LayersWindow',
   Template: 'resource:///io/github/dubstar_04/design/ui/layers/layersWindow.ui',
-  InternalChildren: ['layerList', 'stack', 'backButton', 'nameEntry', 'frozenSwitch', 'lockedSwitch', 'lineType', 'lineWeightLabel', 'plottingSwitch'],
-}, class LayersWindow extends Adw.ApplicationWindow {
+  InternalChildren: ['navigation', 'layerList', 'nameEntry', 'visibilitySwitch', 'frozenSwitch', 'lockedSwitch', 'lineType', 'lineWeightLabel', 'plottingSwitch', 'editLayerPage'],
+}, class LayersWindow extends Adw.Window {
   constructor() {
     super({});
 
     this.loading = true;
     this.selected_layer;
 
-    // Action to edit layers
-    const layerEditAction = new Gio.SimpleAction({
-      name: 'layerEditAction',
-      parameter_type: GLib.VariantType.new('s'),
-    });
-    layerEditAction.connect('activate', this.onEditAction.bind(this));
-    this.add_action(layerEditAction);
-
-    // Action to delete layers
-    const layerDeleteAction = new Gio.SimpleAction({
-      name: 'layerDeleteAction',
-      parameter_type: GLib.VariantType.new('s'),
-    });
-    layerDeleteAction.connect('activate', this.onDeleteAction.bind(this));
-    this.add_action(layerDeleteAction);
-
-    // Action to make layers current
-    const layerCurrentAction = new Gio.SimpleAction({
-      name: 'layerCurrentAction',
-      parameter_type: GLib.VariantType.new('s'),
-    });
-    layerCurrentAction.connect('activate', this.onCurrentAction.bind(this));
-    this.add_action(layerCurrentAction);
+    // Reload layers when navigating back to the layer list
+    this._navigation.connect('popped', this.reload.bind(this));
   }
 
   show() {
     this.present();
-    this.reload();
-  }
-
-  onEditAction(simpleAction, parameters) {
-    const layerName = parameters.deep_unpack();
-    this.selected_layer = DesignCore.LayerManager.getItemByName(layerName);
-    this.onEditLayer();
-  }
-
-  onDeleteAction(simpleAction, parameters) {
-    // console.log("delete action")
-    const layerName = parameters.deep_unpack();
-    this.selected_layer = DesignCore.LayerManager.getItemByName(layerName);
-    this.onLayerDelete();
-  }
-
-  onCurrentAction(simpleAction, parameters) {
-    // console.log("current action")
-    const layerName = parameters.deep_unpack();
-    DesignCore.LayerManager.setCstyle(layerName);
     this.reload();
   }
 
@@ -124,33 +83,20 @@ export const LayersWindow = GObject.registerClass({
     const clayer = DesignCore.LayerManager.getCstyle();
 
     for (let i = 0; i < layers.length; i++) {
+      const row = new PreferenceRow();
+      row.title = layers[i].name;
+      row.set_current(layers[i].name === clayer);
+      this._layerList.append(row);
+
+      row.connect('default-changed', this.setCurrentLayer.bind(this));
+      row.connect('edit-style', this.onEditLayer.bind(this));
+      row.connect('delete-style', this.onLayerDelete.bind(this));
+      row.connect('activated', this.onEditLayer.bind(this));
+
       const colourButton = new Gtk.ColorButton({ 'valign': Gtk.Align.CENTER, 'rgba': this.toRgba(layers[i].colour) });
       colourButton.connect('color-set', this.onColourChange.bind(this));
 
-      const layerSwitch = new Gtk.Switch({ valign: Gtk.Align.CENTER, active: layers[i].on });
-      layerSwitch.connect('state-set', this.onToggled.bind(this));
-
-      const menu = new Gio.Menu();
-      menu.append(_('Edit'), `win.layerEditAction("${layers[i].name}")`);
-      menu.append(_('Delete'), `win.layerDeleteAction("${layers[i].name}")`);
-      menu.append(_('Make Current'), `win.layerCurrentAction("${layers[i].name}")`);
-      const appMenu = Gtk.PopoverMenu.new_from_model(menu);
-
-      const menuButton = new Gtk.MenuButton({ popover: appMenu, valign: Gtk.Align.CENTER, icon_name: 'view-more-symbolic', css_classes: ['flat'] });
-
-
-      const row = new Adw.ActionRow({ title: layers[i].name, activatable: true });
-      row.connect('activated', this.onLayerSelected.bind(this));
-
       row.add_prefix(colourButton);
-      row.add_suffix(layerSwitch);
-      row.add_suffix(menuButton);
-
-      if (layers[i].name === clayer) {
-        row.set_icon_name('adw-entry-apply-symbolic');
-      }
-
-      this._layerList.append(row);
     }
   }
 
@@ -166,42 +112,28 @@ export const LayersWindow = GObject.registerClass({
     this.get_transient_for().getActiveCanvas().queue_draw();
   }
 
-  onToggled(toggle, state) {
-    // Get the row of the switch
-    const row = toggle.get_ancestor(Adw.ActionRow);
-    // get the layer reference from the layer manager
-    const layer = DesignCore.LayerManager.getItemByName(row.title);
-    // change the layer state
-    layer.on = state;
-    // redraw
-    this.get_transient_for().getActiveCanvas().queue_draw();
-  }
-
-  onBackClicked() {
-    this._stack.set_visible_child_name('layerListPage');
-    this._backButton.visible = false;
-    this.reload();
-  }
-
   onNewClicked() {
     // console.log("new clicked")
     DesignCore.LayerManager.newItem();
     this.reload();
   }
 
-  onLayerSelected(row) {
+  setCurrentLayer(row) {
     if (row) {
-      this._layerList.unselect_row(row);
-      this.selected_layer = DesignCore.LayerManager.getItemByName(row.title);
-      this.onEditLayer();
+      DesignCore.LayerManager.setCstyle(row.title);
+      this.reload();
     }
   }
 
-  onEditLayer() {
+  onEditLayer(row) {
+    // set selected layer
+    this.selected_layer = DesignCore.LayerManager.getItemByName(row.title);
+
     // set loading state to prevent layer changes while setting widget state
     this.loading = true;
 
     this._nameEntry.text = this.selected_layer.name;
+    this._visibilitySwitch.active = this.selected_layer.on;
     this._frozenSwitch.active = this.selected_layer.frozen;
     this._lockedSwitch.active = this.selected_layer.locked;
 
@@ -217,8 +149,8 @@ export const LayersWindow = GObject.registerClass({
     this._lineWeightLabel.label = this.selected_layer.lineWeight.toString();
     this._plottingSwitch.active = this.selected_layer.plotting;
 
-    this._stack.set_visible_child_name('LayerDetailsPage');
-    this._backButton.visible = true;
+    // show the edit layer page
+    this._navigation.push(this._editLayerPage);
 
     this.loading = false;
   }
@@ -232,6 +164,7 @@ export const LayersWindow = GObject.registerClass({
 
     const layerIndex = DesignCore.LayerManager.getItemIndex(this.selected_layer.name);
     DesignCore.LayerManager.renameStyle(layerIndex, this._nameEntry.text);
+    this.selected_layer.on = this._visibilitySwitch.active;
     this.selected_layer.frozen = this._frozenSwitch.active;
     this.selected_layer.locked = this._lockedSwitch.active;
     const selectedLineType = this._lineType.get_selected_item().get_string();
@@ -239,32 +172,40 @@ export const LayersWindow = GObject.registerClass({
 
     this.selected_layer.plotting = this._plottingSwitch.active;
     this.get_transient_for().getActiveCanvas().queue_draw();
+    // this.reload();
   }
 
-  onLayerDelete() {
-    // console.log("delete")
-    const dialog = new Adw.MessageDialog();
-    dialog.set_transient_for(this);
-    dialog.set_heading('Delete layer?');
-    dialog.set_body('Delete layer: ' + this.selected_layer.name + ' ?');
-    dialog.add_response('cancel', 'Cancel');
-    dialog.add_response('delete', 'Delete');
-    dialog.set_response_appearance('delete', Adw.ResponseAppearance.DESTRUCTIVE);
-    dialog.connect('response', this.onConfirmDialog.bind(this));
-    dialog.present();
+
+  onLayerDelete(row) {
+    if (row) {
+      const dialog = new Adw.AlertDialog({
+        heading: 'Delete Layer?',
+        body: `Delete Layer: ${row.title}?`,
+        close_response: 'cancel',
+      });
+
+      dialog.add_response('cancel', 'Cancel');
+      dialog.add_response('delete', 'Delete');
+
+      // Make the delete response style destructive
+      dialog.set_response_appearance('delete', Adw.ResponseAppearance.DESTRUCTIVE);
+
+      const parent = this.get_ancestor(Adw.PreferencesDialog);
+      dialog.connect('response', this.onConfirmDialog.bind(this, row));
+      dialog.present(parent);
+    }
   }
 
-  onConfirmDialog(dialog, response) {
-    // console.log("delete dialog callback")
+  onConfirmDialog(row, dialog, response) {
     if (response === 'delete') {
-      this.deleteStyle(this.selected_layer.name);
-      this.onBackClicked();
+      this.deleteStyle(row.title);
     }
   }
 
   deleteStyle(layerName) {
     // console.log("delete layer")
     DesignCore.LayerManager.deleteStyle(DesignCore.LayerManager.getItemIndex(layerName));
+    this.reload();
     this.get_transient_for().getActiveCanvas().queue_draw();
   }
 }, // window
