5f5428ebf97ddb77959d0f245a3ae74168435caf
Daniel Wood <s.d.wood.82@googlemail.com>
2025-11-14 16:46:01 +0000

    fix function key behaviour

--- src/Design/window.js
+++ src/Design/window.js
@@ -140,7 +140,10 @@ export const DesignWindow = GObject.registerClass({
     const toggleOrthoShortcut = new Gtk.Shortcut({ trigger: Gtk.ShortcutTrigger.parse_string('F8'), action: Gtk.CallbackAction.new(this.settings.onSettingToggled.bind(this.settings, 'ortho')) });
     shortcutController.add_shortcut(toggleOrthoShortcut);
 
-    const togglePolarShortcut = new Gtk.Shortcut({ trigger: Gtk.ShortcutTrigger.parse_string('F9'), action: Gtk.CallbackAction.new(this.settings.onSettingToggled.bind(this.settings, 'polar')) });
+    const toggleSnapShortcut = new Gtk.Shortcut({ trigger: Gtk.ShortcutTrigger.parse_string('F9'), action: Gtk.CallbackAction.new(this.onToggleSnapMode.bind(this)) });
+    shortcutController.add_shortcut(toggleSnapShortcut);
+
+    const togglePolarShortcut = new Gtk.Shortcut({ trigger: Gtk.ShortcutTrigger.parse_string('F10'), action: Gtk.CallbackAction.new(this.settings.onSettingToggled.bind(this.settings, 'polar')) });
     shortcutController.add_shortcut(togglePolarShortcut);
 
     this.add_controller(shortcutController);
