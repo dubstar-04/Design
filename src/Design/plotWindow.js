64c743bec728ba1014c3e633a50b321891f15f15
Daniel Wood <s.d.wood.82@googlemail.com>
2026-04-17 17:33:50 +0100

    Add plot window

---
+++ src/Design/plotWindow.js
@@ -0,0 +1,94 @@
+/* PlotWindow.js
+ *
+ * Copyright 2024 Daniel Wood
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import GObject from 'gi://GObject';
+import Adw from 'gi://Adw?version=1';
+import Gtk from 'gi://Gtk';
+
+import { Constants } from '../Design-Core/core/lib/constants.js';
+import { RendererBase } from '../Design-Core/core/lib/renderers/rendererBase.js';
+import { FileIO } from './fileIO.js';
+
+/** Scale label → numeric value (or 'fit' sentinel). */
+const scaleMap = {
+  'Fit': 'fit',
+  '1:1': 1,
+  '1:2': 0.5,
+  '1:5': 0.2,
+  '1:10': 0.1,
+  '2:1': 2,
+  '5:1': 5,
+};
+
+/** File type label → extension string. */
+const fileTypeMap = {
+  'PDF': 'pdf',
+  'SVG': 'svg',
+};
+
+/** Plot style label → RendererBase.Styles transform. */
+const styleMap = {
+  'None': RendererBase.Styles.NONE,
+  'Monochrome': RendererBase.Styles.MONOCHROME,
+  'Greyscale': RendererBase.Styles.GREYSCALE,
+};
+
+export const PlotWindow = GObject.registerClass({
+  GTypeName: 'PlotWindow',
+  Template: 'resource:///io/github/dubstar_04/design/ui/plot.ui',
+  InternalChildren: ['pageSize', 'orientation', 'plotArea', 'plotScale', 'plotStyle', 'fileType'],
+}, class PlotWindow extends Adw.Window {
+  show() {
+    // Populate page size combo from Constants.PageSizes keys
+    const sizeNames = Object.keys(Constants.PageSizes);
+    this._pageSize.model = new Gtk.StringList({ strings: sizeNames });
+    this._pageSize.selected = 0;
+
+    // Default to Landscape (index 1)
+    this._orientation.selected = 1;
+
+    this.present();
+  }
+
+  /** Build the options object from the current combo-row selections. */
+  #buildOptions() {
+    const sizeName = this._pageSize.model.get_string(this._pageSize.selected);
+    const pageSize = Constants.PageSizes[sizeName];
+    const isLandscape = this._orientation.selected === 1;
+
+    const pageWidth = isLandscape ? pageSize.height : pageSize.width;
+    const pageHeight = isLandscape ? pageSize.width : pageSize.height;
+
+    const scaleLabel = this._plotScale.model.get_string(this._plotScale.selected);
+    const plotScale = scaleMap[scaleLabel] ?? 'fit';
+
+    const styleLabel = this._plotStyle.model.get_string(this._plotStyle.selected);
+    const style = styleMap[styleLabel] ?? RendererBase.Styles.NONE;
+
+    const fileTypeLabel = this._fileType.model.get_string(this._fileType.selected);
+    const fileType = fileTypeMap[fileTypeLabel] ?? 'pdf';
+
+    return { pageWidth, pageHeight, plotScale, style, fileType };
+  }
+
+  onExportClicked() {
+    const mainWindow = this.get_transient_for();
+    FileIO.exportPlot(mainWindow, this.#buildOptions());
+    this.close();
+  }
+});
